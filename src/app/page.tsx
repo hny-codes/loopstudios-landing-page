527c2190c3400fdf7e8228bf24237508d8c43a4b
Henry <hnguyhenry@protonmail.com>
2023-10-16 14:54:24 -0700

    Add navbar client component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,11 @@
+import Navbar from "@/components/Navbar";
+
 export default function Home() {
-  return <main className=''></main>;
+  return (
+    <main className=''>
+      <div>
+        <Navbar />
+      </div>
+    </main>
+  );
 }
