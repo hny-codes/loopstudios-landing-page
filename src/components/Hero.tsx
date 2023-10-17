f1281c443916a345a41c09ff580d58991f46f074
Henry <hnguyhenry@protonmail.com>
2023-10-17 14:35:13 -0700

    Edit html to be semantic

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -3,11 +3,13 @@ export default function Hero() {
     <div
       className={`bg-[url('/images/mobile/image-hero.jpg')] sm:bg-[url('/images/desktop/image-hero.jpg')] bg-cover bg-no-repeat bg-center h-[650px] sm:h-[550px] text-[--clr-white] flex flex-col justify-center `}
     >
-      <div className='max-w-[--max-width] mx-auto w-full'>
-        <h1 className='border-2 px-8 py-6 text-4xl sm:text-6xl mx-6 [word-spacing:9999px] sm:[word-spacing:0px] overflow-hidden font-josefin sm:max-w-[33rem] sm:mx-6 uppercase  tracking-wide'>
-          Immersive Experiences That Deliver
-        </h1>
-      </div>
+      <section className='max-w-[--max-width] mx-auto w-full'>
+        <header>
+          <h1 className='border-2 px-8 py-6 text-4xl sm:text-6xl mx-6 [word-spacing:9999px] sm:[word-spacing:0px] overflow-hidden font-josefin sm:max-w-[33rem] sm:mx-6 uppercase  tracking-wide'>
+            Immersive Experiences That Deliver
+          </h1>
+        </header>
+      </section>
     </div>
   );
 }
