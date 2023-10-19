8a5e290edb4cc9a63cca2c62150d29de76cc15f0
Henry <hnguyhenry@protonmail.com>
2023-10-18 20:23:55 -0700

    Add button component

---
+++ src/components/Button.tsx
@@ -0,0 +1,11 @@
+type Prop = {
+  text: string;
+};
+
+export default function Button({ text }: Prop) {
+  return (
+    <button className='uppercase tracking-widest border-2 border-[--clr-black] px-12 py-2 font-josefin hover:bg-[--clr-black] hover:text-[--clr-white]'>
+      <p>{text}</p>
+    </button>
+  );
+}
