f8884820f0b238904b5d329310c90d3bb340bc2b
Henry <hnguyhenry@protonmail.com>
2023-10-20 22:54:54 -0700

    Add variant for menu items

--- src/util/framerAnimation.ts
+++ src/util/framerAnimation.ts
@@ -6,6 +6,32 @@ export const heroVariant: Variants = {
 };
 
 export const navVariant: Variants = {
-  hidden: { opacity: 0 },
-  visible: { opacity: 1 },
+  hidden: { opacity: 0, scale: 0 },
+  visible: {
+    opacity: 1,
+    transition: {
+      staggerChildren: 0.1,
+      delayChildren: 0.3,
+      type: 'tween'
+    },
+    scale: 1,
+    rotate: 360,
+  },
+};
+
+export const menuItemVariant: Variants = {
+  hidden: {
+    opacity: 0,
+    x: 300,
+    transition: {
+      type: 'tween',
+    },
+  },
+  visible: {
+    opacity: 1,
+    x: 0,
+    transition: {
+      type: 'tween',
+    },
+  },
 };
