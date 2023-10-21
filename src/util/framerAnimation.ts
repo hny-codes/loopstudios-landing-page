2a44ca918d92869737ae5d98076caba6a7d15df9
Henry <hnguyhenry@protonmail.com>
2023-10-20 22:21:37 -0700

    Add framer animation util file

---
+++ src/util/framerAnimation.ts
@@ -0,0 +1,11 @@
+import { Variants } from 'framer-motion';
+
+export const heroVariant: Variants = {
+  hidden: { opacity: 0 },
+  visible: { opacity: 1 },
+};
+
+export const navVariant: Variants = {
+  hidden: { opacity: 0 },
+  visible: { opacity: 1 },
+};
