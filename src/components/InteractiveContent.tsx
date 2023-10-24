440177804801b509f5698c8fadf0c26344991f0c
Henry <hnguyhenry@protonmail.com>
2023-10-23 21:38:31 -0700

    Add animation to content section

--- src/components/InteractiveContent.tsx
+++ src/components/InteractiveContent.tsx
@@ -1,7 +1,20 @@
+'use client';
+
+import { motion } from 'framer-motion';
+import { useInView } from 'framer-motion';
+import { useRef } from 'react';
+
 export default function InteractiveContent() {
+  const ref = useRef(null);
+  const inView = useInView(ref, { once: true, amount: 0.8 });
+
   return (
-    <header
-      className='text-center sm:text-left mt-12 bg-[--clr-white] sm:w-2/3 md:w-1/2 sm:px-20 sm:pt-20 sm:ml-auto
+    <motion.header
+      ref={ref}
+      initial={{ opacity: 0 }}
+      id='interactive-text'
+      animate={inView && { opacity: 1 }}
+      className='text-center sm:text-left mt-12 sm:bg-[--clr-white] sm:w-2/3 md:w-1/2 sm:px-20 sm:pt-20 sm:ml-auto
         sm:absolute right-0 bottom-0'
     >
       <h2 className='uppercase text-3xl sm:text-4xl md:text-5xl tracking-wide font-josefin'>
@@ -13,6 +26,6 @@ export default function InteractiveContent() {
         award-winning creations have transformed businesses through digital
         experiences that bind to their brand.
       </p>
-    </header>
+    </motion.header>
   );
 }
