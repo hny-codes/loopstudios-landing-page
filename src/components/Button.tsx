a0272af7a3e384f4018331eb8d16d240311c8984
Henry <hnguyhenry@protonmail.com>
2023-10-25 19:59:40 -0700

    Add transition

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -4,7 +4,7 @@ type Prop = {
 
 export default function Button({ text }: Prop) {
   return (
-    <button className='uppercase tracking-widest border-2 border-[--clr-black] px-12 py-2 font-josefin hover:bg-[--clr-black] hover:text-[--clr-white]'>
+    <button className='uppercase tracking-widest border-2 border-[--clr-black] px-12 py-2 font-josefin hover:bg-[--clr-black] hover:transition hover:text-[--clr-white]'>
       <p>{text}</p>
     </button>
   );
